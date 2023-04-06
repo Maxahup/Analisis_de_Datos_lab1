183536f9eb5aa6ca8602fc55db1bfb5b14273880
Maximiliano Ahumada <64162650+Maxahup@users.noreply.github.com>
2023-04-06 10:41:26 -0400

    Creación script
    lectura de archivos

---
+++ Script.r
@@ -0,0 +1,14 @@
+#Autores: Daniela Orellana - Maximiliano Ahumada
+#Laboratorio 1 Análisis de Datos
+
+
+#Se fija el espacio de trabajo
+#setwd("")
+
+#Apertura de bases de datos procesadas
+clevelandPD <- read.csv(file.choose())
+hungarianPD <- read.csv(file.choose())
+switzerlandPD <- read.csv(file.choose())
+vaPD <- read.csv(file.choose())
+
+
